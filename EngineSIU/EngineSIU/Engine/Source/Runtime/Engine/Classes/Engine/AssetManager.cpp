d260c84caa90af7455a462b37956696c8d326f0b
Mstone8370 <mstone8370@gmail.com>
2025-04-19 12:11:14 +0900

    feat: 로딩 오래걸리는 메시 제외

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/AssetManager.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/AssetManager.cpp
@@ -50,6 +50,12 @@ void UAssetManager::LoadObjFiles()
     {
         if (Entry.is_regular_file() && Entry.path().extension() == ".obj")
         {
+            if (Entry.path().filename() != "sponza-atrium-3.obj")
+            {
+                // TODO: 임시로 큰 파일은 제외했으므로, 필요한 경우 이 분기점 제거
+                continue;
+            }
+            
             FAssetInfo NewAssetInfo;
             NewAssetInfo.AssetName = FName(Entry.path().filename().string());
             NewAssetInfo.PackagePath = FName(Entry.path().parent_path().string());
